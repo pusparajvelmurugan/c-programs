c4638cd5b48ba4fc7dbab1096b6897e31e62e23d
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2017-08-08 15:55:55 +0530

    alpha
    this is a code

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrcsr();
+printf("enter the valueof a"):
+scanf("%s",a);
+}
+if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+{
+printf("the given a value is an albhabet");
+else
+printf("given a value is not an albhabet");
+getch();
+}
